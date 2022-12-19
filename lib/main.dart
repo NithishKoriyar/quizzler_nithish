c7627b779fa1046b09d70943815092f844393b39
Nithish <nithishk6765@gmail.com>
2022-12-19 14:24:41 +0530

    changing file content in main dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
-
-
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
+        backgroundColor: Color.fromARGB(255, 222, 218, 218),
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
@@ -23,45 +20,20 @@ class Quizzler extends StatelessWidget {
 }
 
 class QuizPage extends StatefulWidget {
-
-
-
   @override
   _QuizPageState createState() => _QuizPageState();
 }
 
 class _QuizPageState extends State<QuizPage> {
-
-    List<Icon> scoreKeeper = [
-     Icon(
-              Icons.check,
-              color: Colors.red,
-            ),
-
-            Icon(
-              Icons.close,
-              color: Colors.orange,
-            ),   
-
-            Icon(
-              Icons.close,
-              color: Colors.orange,
-            ),        
-
-
-            Icon(
-              Icons.close,
-              color: Colors.orange,
-            ),        
-
-
-            Icon(
-              Icons.close,
-              color: Colors.orange,
-            ),        
-
-
+  List<Icon> scorkeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.?',
+    'Approximately one quarter of human bones are in the feet.?',
+    'A slug\'s blood is green.?',
   ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -74,11 +46,11 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
-                  color: Colors.white,
+                  color: Color.fromARGB(255, 3, 3, 3),
                 ),
               ),
             ),
@@ -89,8 +61,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(15.0),
             child: TextButton(
               style: ButtonStyle(
-             backgroundColor: MaterialStatePropertyAll<Color>(Colors.green),
-             ),            
+                  backgroundColor: MaterialStatePropertyAll(Colors.green)),
               child: Text(
                 'True',
                 style: TextStyle(
@@ -101,13 +72,9 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.blue,
-                    ),
-                  );
+                  questionNumber++;
                 });
+                print(questionNumber);
               },
             ),
           ),
@@ -116,9 +83,8 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: TextButton(
-               style: ButtonStyle(
-             backgroundColor: MaterialStatePropertyAll<Color>(Colors.yellow),
-             ),   
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.red)),
               child: Text(
                 'False',
                 style: TextStyle(
@@ -127,25 +93,18 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.white,
-                    ),
-                  );
-                });
                 //The user picked false.
+                setState(() {
+                  questionNumber++;
+                });
+                print(questionNumber);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
-        
-
         Row(
-          children: scoreKeeper,
-        ),      
+          children: scorkeeper,
+        )
       ],
     );
   }
